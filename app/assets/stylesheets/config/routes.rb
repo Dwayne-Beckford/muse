d4e08cbfa3627b15d8f2fd5bf00ae294582c0690
Diana Al-Zubi <dianachka2008@gmail.com>
2025-05-28 12:11:08 +0100

    deleted unnecessary config file that was created earlier by mistake

--- app/assets/stylesheets/config/routes.rb
+++
@@ -1,7 +0,0 @@
-#config/routes.rb
-# [...]
-Rails.application.routes.draw do
-  resources :artworks
-end
-# The `new` route needs to be placed before the `show` route
-# get "artworks/new", to:"artworks#new"
